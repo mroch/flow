2dd0727a583c82f969214a69914f78e8f22c780c
Marshall Roch <mroch@fb.com>
2018-01-24 00:09:09 -0800

    Add initial circleci integration

---
+++ .circleci/github_upload.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -H "Authorization: token $DOC_BOT_TOKEN" \
+  -H "Accept: application/vnd.github.manifold-preview" \
+  -H "Content-Type: application/zip" \
+  --data-binary @$1 \
+  "https://uploads.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/releases/$CIRCLE_TAG/assets?name=$2"
